fa9bf4f2c349b11fb830e72246cd6499f6630c61
䷳ Gradual Progress | 漸 |Development| <truck@notonfire.co.uk>
2015-08-22 15:10:42 +0300

    Verified I know how to pass variables
     - also learned syntax for unpacking a compacted variable array.

--- BACKEND/resources/views/static/about.blade.php
+++ BACKEND/resources/views/static/about.blade.php
@@ -5,7 +5,17 @@
 @stop
 
 @section('content')
-	<h2>this is our game and stuff</h2>
+	<h2>this is our game called '{{ $stuff['Game_Name'] }}' and stuff</h2>
+	<h3>and by 'our' we mean:
+	<ul>
+	@forelse ($stuff['Authors'] as $author)
+		<li>{{$author}}</li>
+	@empty
+		<li>NOBODY OMG NO ONE</li>
+	@endforelse
+
+	</ul>
+	</h3>
 	<p>I'm actually just making sure routing works and being waay too verbose</p>
 	<p>aaand that was Truck's lesson in 'oh yeah this is how css works' for today</p>
 @stop
